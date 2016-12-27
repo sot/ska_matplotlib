590329b672a51664860ed3ec24d1a8c9d4c9bb87
Tom Aldcroft <taldcroft@gmail.com>
2016-12-27 11:10:48 -0500

    Update tests for minimal validation of plot_cxctime and plot_line_ids

--- test.py
+++ test.py
@@ -1,38 +1,42 @@
 from __future__ import print_function, division
 
-import numpy as np
-from Ska.Matplotlib import plot_cxctime, cxctime2plotdate, set_time_ticks
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+import numpy as np
+from Ska.Matplotlib import plot_cxctime
+from Ska.Matplotlib.lineid_plot import plot_line_ids
+import pytest
 
-def test_non_interactive():
-    x = 4e8 + np.arange(1000) / 200.
-    y = x - 4e8
-    plot_cxctime(x, y)
-    plt.savefig('test.png')
+np.random.seed(1)
 
 
-def test_check_many_sizes():
-    """Run through a multiplicative series of x-axis lengths and visually
-    confirm that chosen axes are OK."""
-    plt.ion()
-    dt = 6
-    while True:
-        print(dt)
-        t0 = np.random.uniform(3e7 * 10)
-        times = np.linspace(t0, t0 + dt, 20)
-        x = cxctime2plotdate(times)
-        y = np.random.normal(size=len(times))
+dts = 6 * 2 ** np.arange(28)  # Max of about 25 years
 
-        fig = plt.figure(1)
-        fig.clf()
-        plt1 = fig.add_subplot(1, 1, 1)
-        plt1.plot_date(x, y, fmt='b-')
 
-        set_time_ticks(plt1)
+@pytest.mark.parametrize('dt', dts)
+def test_check_many_sizes(dt):
+    """
+    Run through a multiplicative series of x-axis lengths and visually
+    confirm that chosen axes are OK.
+    """
+    plt.close(0)
+    plt.figure(0)
 
-        fig.autofmt_xdate()
-        plt.savefig('test-{:09d}.png'.format(dt))
-        dt *= 2
-        if dt > 1e9:
-            break
+    t0 = np.random.uniform(3e7 * 10)
+    times = np.linspace(t0, t0 + dt, 20)
+    y = np.random.normal(size=len(times))
+    plot_cxctime(times, y)
+
+    plt.savefig('test-{:09d}.png'.format(dt))
+
+
+def test_lineid():
+    """Minimal test of plotting line IDs.  Note the test of duplicate line labels"""
+    plt.figure()
+    plot_line_ids(np.arange(100), np.random.uniform(size=100),
+                  np.linspace(10, 90, 10),
+                  ['line{}'.format(ii) for ii in
+                   (0, 0, 0, 1, 1, 2, 3, 4, 5, 6)])
+    plt.savefig('test-lineid.png')
