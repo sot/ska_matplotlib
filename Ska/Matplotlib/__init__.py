69a47d1a085e730df9ceba61c18bddcfdf3cc590
Javier Gonzalez <javierggt@yahoo.com>
2019-12-17 12:57:36 -0500

    Added changes to use setuptools_scm

--- Ska/Matplotlib/__init__.py
+++ Ska/Matplotlib/__init__.py
@@ -1,4 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import ska_helpers
+
 from .core import *
 
-__version__ = '3.11.2'
+__version__ = ska_helpers.get_version(__package__)
